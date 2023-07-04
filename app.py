0b4df59ecd992db1a968d749972fea0f791d84c8
TriangleBear <kakanini31@gmail.com>
2023-07-04 13:54:07 +0800

    Firebase Keys

--- app.py
+++ app.py
@@ -2,17 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for,session
 import pyrebase
 from requests.exceptions import HTTPError
 import re
-
-firebaseConfig={
-    'apiKey': "AIzaSyAU64526sGBB8ZP-M7mgocQkSbyAFJ3klM",
-    'authDomain': "pixelpix-4dbb5.firebaseapp.com",
-    'databaseURL': "https://pixelpix-4dbb5-default-rtdb.firebaseio.com",
-    'projectId': "pixelpix-4dbb5",
-    'storageBucket': "pixelpix-4dbb5.appspot.com",
-    'messagingSenderId': "574563555513",
-    'appId': "1:574563555513:web:333039f782b9e0b87aa368",
-    'measurementId': "G-XD1HMR66RR"
-}
+from key import firebaseConfig
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
