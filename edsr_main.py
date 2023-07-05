5248cc554aff4bd3cdf5306759d20244ccb9a957
Ryan Caabay <ryanpcaabay9@gmail.com>
2023-07-05 21:33:04 +0800

    Attempt to fix upscale button

--- edsr_main.py
+++ edsr_main.py
@@ -1,23 +1,11 @@
 import cv2
 from cv2 import dnn_superres
 
-# EDSR
-# initialize super resolution object
-sr = dnn_superres.DnnSuperResImpl_create()
-# read the model
-path = './models/EDSR_x4.pb'
-sr.readModel(path)
-# set the model and scale
-sr.setModel('edsr', 4)
-# load the image
-image = cv2.imread('./images/DIV2K_valid_LR_x8/0817x8.png')
-# upsample the image
-upscaled = sr.upsample(image)
-# save the upscaled image
-cv2.imwrite('./images/output/edsr_upscaled_test_4x.png', upscaled)
-
-# traditional method - bicubic
-bicubic = cv2.resize(image, (upscaled.shape[1], upscaled.shape[0]), interpolation=cv2.INTER_CUBIC)
-# save the image
-cv2.imwrite('./images/output/bicubic_test_4x.png', bicubic)
-
+def edsr(uploadedImage):
+    sr = dnn_superres.DnnSuperResImpl_create()
+    path = './models/EDSR_x4.pb'
+    sr.readModel(path)
+    sr.setModel('edsr', 4)
+    image = cv2.imread(uploadedImage)
+    upscaled = sr.upsample(image)
+    return upscaled
