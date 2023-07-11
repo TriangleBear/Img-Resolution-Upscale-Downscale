911c4a8616c44243b1808a268da75a8bc270340f
NeilMlby <137358479+NeilMlby@users.noreply.github.com>
2023-07-11 14:50:34 +0800

    Neil test (#3)
    * Added training folders
    
    * updated-home-screen
    
    * updated upscale and upload display
    
    * Attempt to fix upscale button
    
    * Revert "Attempt to fix upscale button"
    
    This reverts commit 5248cc554aff4bd3cdf5306759d20244ccb9a957.
    
    * Default
    
    This is where we revert to when shit happens. Time to go ham and fix this overnight.
    
    * First fix attempt .js to .py
    
    Attempt to call edsr() from edsr_main.py within upscaleImage() from main.js
    
    * Revert "First fix attempt .js to .py"
    
    This reverts commit 2d9b4288514b1dd1a4e434e4295ea51b223979ef.
    
    * Second fix attempt
    
    * Default no. 2
    
    * Update edsr_main.py
    
    * Third attempt
    
    * Updated
    
    * Fixed upscale button
    
    * Directory cleaning
    
    * Removed unused files and imports
    
    * Cleaning directory
    
    * Refactored code
    
    * Created requirements.txt
    
    * Refactored code
    
    * Refactored code
    
    * Updated requirements.txt
    
    * download button working
    
    * displayed upload and upscaled
    
    Changed the description in home screen
    
    * UI ongoing
    
    ongoing
    
    * UI ongoing
    
    ongoing
    
    * New UI Update
    
    Update
    
    ---------
    
    Co-authored-by: ryancaabay <67262065+ryancaabay@users.noreply.github.com>
    Co-authored-by: imkjas <90836502+imkjas@users.noreply.github.com>
    Co-authored-by: imkjas <julieann.silao27@gmail.com>
    Co-authored-by: Ryan Caabay <ryanpcaabay9@gmail.com>

--- app.py
+++ app.py
@@ -1,125 +1,171 @@
-from flask import Flask, render_template, request, redirect, url_for,session
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    make_response,
+)
+from key import firebaseConfig, secret_key
+
+import io
+
+import numpy as np
+from cv2 import dnn_superres
+from PIL import Image
+import base64
 import pyrebase
-from requests.exceptions import HTTPError
 import re
-from key import firebaseConfig
+from requests.exceptions import HTTPError
+
+
+
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
 app = Flask(__name__)
+app.secret_key = secret_key
+
 
 def check_user_logged_in():
-    if 'user_id' in session:
+    if "user_id" in session:
         return True
     else:
         return False
 
 
-@app.route('/')
+@app.route("/upscale", methods=["POST"])
+def upscale_image():
+    uploaded_image = request.form["imageData"]
+    uploaded_image = uploaded_image.replace("data:image/png;base64,", "")
+
+    imgdata = base64.b64decode(uploaded_image)
+    img = Image.open(io.BytesIO(imgdata))
+
+    sr = dnn_superres.DnnSuperResImpl_create()
+    path = "./model/EDSR_x4.pb"
+    sr.readModel(path)
+    sr.setModel("edsr", 4)
+
+    upscaled_image = sr.upsample(np.array(img)[:, :, :-1])
+    upscaled_image = Image.fromarray(upscaled_image)
+    buffered = io.BytesIO()
+    upscaled_image.save(buffered, format="JPEG")
+    upscaled_image_64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    response = make_response(upscaled_image_64)
+
+    return response
+
+
+@app.route("/")
 def home():
     user_logged_in = check_user_logged_in()
 
-    return render_template('home_screen.html', user_logged_in=user_logged_in)
+    return render_template("home_screen.html", user_logged_in=user_logged_in)
 
 
-@app.route('/go_premium')
+@app.route("/go_premium")
 def go_premium():
-    user_logged_in = check_user_logged_in() 
+    user_logged_in = check_user_logged_in()
 
-    return render_template('go_premium.html', user_logged_in=user_logged_in)
+    return render_template("go_premium.html", user_logged_in=user_logged_in)
 
 
-@app.route('/login_screen', methods=['GET', 'POST'])
+@app.route("/login_screen", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
         try:
             login = auth.sign_in_with_email_and_password(email, password)
-            session['user_id'] = login['idToken']
-            return redirect(url_for('home'))
+            session["user_id"] = login["idToken"]
+            return redirect(url_for("home"))
         except:
-            error_message = "Invalid email or password"
-            return render_template('login_screen.html', error_message=error_message)
+            error_message = "Incorrect email or password."
+            return render_template("login_screen.html", error_message=error_message)
 
-    return render_template('login_screen.html')
+    return render_template("login_screen.html")
 
-@app.route('/register_screen', methods=['GET', 'POST'])
+
+@app.route("/register_screen", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        username = request.form.get('username')
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        username = request.form.get("username")
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+
         if not username or not first_name or not last_name:
-            error_message = 'Please fill in all the required fields.'
-            return render_template('register_screen.html', error_message=error_message)
+            error_message = "Please fill in all the required fields."
+            return render_template("register_screen.html", error_message=error_message)
 
-        if len(password) <6:
-            error_message = 'Password should be at least 6 characters.'
-            return render_template ('register_screen.html', error_message=error_message)
-        
+        if len(password) < 6:
+            error_message = "Password should be at least 6 characters."
+            return render_template("register_screen.html", error_message=error_message)
 
-        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-        
+        email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
         if not re.match(email_pattern, email):
-            error_message = 'Invalid email format.'
-            return render_template('register_screen.html', error_message=error_message)
+            error_message = "Invalid email format."
+            return render_template("register_screen.html", error_message=error_message)
 
-        # Regular expression pattern for password conditions
-        password_pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$"
-        
+        password_pattern = (
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$"
+        )
+
         if not re.match(password_pattern, password):
-            error_message = 'Password should contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number.'
-            return render_template('register_screen.html', error_message=error_message)
+            error_message = "Password should contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."
+            return render_template("register_screen.html", error_message=error_message)
 
         try:
             user = auth.create_user_with_email_and_password(email, password)
-            return render_template('login_screen.html')
+            return render_template("login_screen.html")
         except HTTPError as e:
             if e.response is not None and e.response.content:
-                error_message = e.response.json()['error']['message']
+                error_message = e.response.json()["error"]["message"]
             else:
-                error_message = 'An error occurred during registration.'
-            return render_template('register_screen.html', error_message=error_message)
+                error_message = "An error occurred during registration."
+            return render_template("register_screen.html", error_message=error_message)
 
-    return render_template('register_screen.html')
+    return render_template("register_screen.html")
 
 
-@app.route('/reset_password', methods=['POST'])
+@app.route("/reset_password", methods=["POST"])
 def reset_password():
-    email = request.form.get('email')
-    
+    email = request.form.get("email")
+
     try:
         auth.send_password_reset_email(email)
-        return render_template('forgot_password.html', reset_success=True)
+        return render_template("forgot_password.html", reset_success=True)
     except:
         error_message = "Failed to send password reset email. Please try again."
-        return render_template('forgot_password.html', error_message=error_message)
+        return render_template("forgot_password.html", error_message=error_message)
 
-@app.route('/forgotpass', methods=['GET', 'POST'])
+
+@app.route("/forgotpass", methods=["GET", "POST"])
 def forgotpass():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        
+    if request.method == "POST":
+        email = request.form.get("email")
+
         try:
             auth.send_password_reset_email(email)
-            return render_template('forgot_password.html', reset_success=True)
+            return render_template("forgot_password.html", reset_success=True)
         except:
             error_message = "Failed to send password reset email. Please try again."
-            return render_template('forgot_password.html', error_message=error_message)
-    
-    return render_template('forgot_password.html')
+            return render_template("forgot_password.html", error_message=error_message)
+
+    return render_template("forgot_password.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('home'))
+    session.pop("user_id", None)
+    return redirect(url_for("home"))
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/JS/main.js
+++ static/JS/main.js
@@ -2,56 +2,48 @@ var uploadedImage;
 var upscaledImage;
 
 function updateUploadStatus() {
-    var fileInput = document.getElementById('file');
-    var uploadStatus = document.querySelector('.upload-status');
+  var fileInput = document.getElementById("file");
+  var uploadStatus = document.querySelector(".upload-status");
+  var uploadButtons = document.getElementById("upload-buttons");
 
-    if (fileInput.files.length > 0) {
-        var fileSize = fileInput.files[0].size / 1024 / 1024; // size in MB
-        uploadStatus.textContent = 'Photo has been uploaded. ' + fileSize.toFixed(2) + ' MB';
+  if (fileInput.files.length > 0) {
+    var fileSize = fileInput.files[0].size / 1024 / 1024; // size in MB
+    uploadStatus.textContent =
+      "Photo has been uploaded. " + fileSize.toFixed(2) + " MB";
 
-        var reader = new FileReader();
+    var reader = new FileReader();
 
-        reader.onload = function(e) {
-            uploadedImage = new Image();
-            uploadedImage.onload = function() {
-                var imageContainer = document.getElementById('image-container');
-                imageContainer.innerHTML = '';
-                imageContainer.appendChild(uploadedImage);
-            };
+    reader.onload = function (e) {
+      uploadedImage = new Image();
+      uploadedImage.onload = function () {
+        var imageContainer = document.getElementById("image-container");
+        imageContainer.innerHTML = "";
+        imageContainer.appendChild(uploadedImage);
 
-            uploadedImage.src = e.target.result;
-        };
+        document.getElementById("uploaded-column").style.display = "none";
+        document.getElementById("upscaled-column").style.display = "none";
+        uploadButtons.style.display = "block";
+        document.getElementById("download-button").style.display = "none";
 
-        reader.readAsDataURL(fileInput.files[0]);
-    } else {
-        uploadStatus.textContent = '';
-    }
-}
+        // Convert image to base64
+        var canvas = document.createElement("canvas");
+        canvas.width = uploadedImage.width;
+        canvas.height = uploadedImage.height;
 
-function upscaleImage() {
-    if (uploadedImage) {
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
+        var context = canvas.getContext("2d");
+        context.drawImage(uploadedImage, 0, 0);
 
-        // Upscale the image 2x
-        canvas.width = uploadedImage.width * 2;
-        canvas.height = uploadedImage.height * 2;
+        var base64Data = canvas.toDataURL("image/png"); // Change 'image/png' to the desired MIME type if needed
+        localStorage.setItem("imageData", base64Data);
+      };
 
-        ctx.drawImage(uploadedImage, 0, 0, canvas.width, canvas.height);
+      uploadedImage.src = e.target.result;
+    };
 
-        var imageContainer = document.getElementById('image-container');
-        imageContainer.innerHTML = '';
-        imageContainer.appendChild(canvas);
-
-        upscaledImage = canvas.toDataURL();
-    }
-}
-
-function downloadImage() {
-    if (upscaledImage) {
-        var link = document.createElement('a');
-        link.download = 'upscaled-image.png';
-        link.href = upscaledImage;
-        link.click();
-    }
+    reader.readAsDataURL(fileInput.files[0]);
+  } else {
+    uploadStatus.textContent = "";
+    uploadButtons.style.display = "none";
+    document.getElementById("download-button").style.display = "none";
+  }
 }
